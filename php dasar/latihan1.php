59ec0a27a50acdad568df4d53289324d7673d02d
putra <ricoemel30@gmail.com>
2025-04-22 07:55:20 +0700

    php dasar

---
+++ php dasar/latihan1.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "Ini Latihan PHP Pertamaku";
---
+++ php dasar/latihan3_paragraph.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paragraph</title>
+</head>
+
+<body>
+    <?php
+    echo "<p align='center'>Teks ini rata tengah</p>";
+    echo "<p align='right'>Teks ini rata kanan</p>";
+    ?>
+</body>
+
+</html>
