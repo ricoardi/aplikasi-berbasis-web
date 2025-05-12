9ffcf20b61b94c1b20ebdc481be447784a124fc3
putra <ricoemel30@gmail.com>
2025-05-12 12:35:46 +0700

    p

---
+++ tugas-akhir-kel3-php/tutorials/12_concatenatestrings.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h3>Concatenate Strings - 
+Menggabungkan String</h3>
+<br>
+<h5>String Concatenation</h5>
+<?php
+$x = "Hello";
+$y = "World";
+$z = $x . $y;
+echo $z;
+?>
+<hr>
+<?php
+$x = "Hello";
+$y = "World";
+$z = $x . " " . $y;
+echo $z;
+?>
+<hr>
+<?php
+$x = "Hello";
+$y = "World";
+$z = "$x $y";
+echo $z;
+?>
+</body>
+</html>
+
